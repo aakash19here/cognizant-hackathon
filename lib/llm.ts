89188da2653aeed131ee5c82f280bdf505dcb12b
aakash19here <aakash19here@gmail.com>
2023-11-23 23:46:37 +0530

    chore: add model name in chatModel conf

--- lib/llm.ts
+++ lib/llm.ts
@@ -3,6 +3,7 @@ import { env } from "./config";
 
 export const chatModel = new OpenAI({
   openAIApiKey: env.OPEN_AI_API_KEY,
+  modelName: "gpt-3.5-turbo",
   streaming: true,
   verbose: true,
   temperature: 0,
